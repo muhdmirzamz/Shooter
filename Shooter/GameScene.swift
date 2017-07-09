3ad9eae30b4c54480e250b718a71c4059aa67abc
muhdmirzamz <muhdmirzamz@gmail.com>
2017-07-09 20:57:57 +0800

    Monster auto-spawn upon hit

--- Shooter/GameScene.swift
+++ Shooter/GameScene.swift
@@ -42,9 +42,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 		self.playerSprite?.physicsBody?.collisionBitMask = Collision().Border
 		self.playerSprite?.physicsBody?.isDynamic = false
 		
-		let monster = SKSpriteNode.init(imageNamed: "monster")
-		monster.position = CGPoint.init(x: self.size.width - 100, y: self.randYPositionForMonsterWith(height: monster.size.height))
-		self.addChild(monster)
+		self.spawnMonster()
     }
 
 	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,11 +64,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 			
 			projectileSprite.physicsBody = SKPhysicsBody.init(circleOfRadius: projectileSprite.size.width / 2)
 			projectileSprite.physicsBody?.categoryBitMask = Collision().Projectile
+			projectileSprite.physicsBody?.contactTestBitMask = Collision().Monster
 			
 			let actionMove = SKAction.moveTo(x: self.size.width + 10, duration: 1)
 			let actionMoveDone = SKAction.removeFromParent()
 			projectileSprite.run(SKAction.sequence([actionMove, actionMoveDone]))
 		}
+	}
+	
+	public func didBegin(_ contact: SKPhysicsContact) {
+		let projectileBody: SKPhysicsBody
+		let monsterBody: SKPhysicsBody
+		
+		// find body type
+		if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+			projectileBody = contact.bodyA
+			monsterBody = contact.bodyB
+		} else {
+			projectileBody = contact.bodyB
+			monsterBody = contact.bodyA
+		}
+		
+		// type check just to make sure
+		// not enough to merely check for collision value assigned
+		if projectileBody.categoryBitMask == Collision().Projectile && monsterBody.categoryBitMask == Collision().Monster {
+			let action = SKAction.removeFromParent()
+			
+			// access node to apply actions
+			projectileBody.node?.run(action)
+			monsterBody.node?.run(action)
+			
+			self.spawnMonster()
+		}
+	}
+	
+	func spawnMonster() {
+		let monster = SKSpriteNode.init(imageNamed: "monster")
+		monster.position = CGPoint.init(x: self.size.width - 100, y: self.randYPositionForMonsterWith(height: monster.size.height))
+		self.addChild(monster)
+		
+		monster.physicsBody = SKPhysicsBody.init(rectangleOf: monster.size)
+		monster.physicsBody?.categoryBitMask = Collision().Monster
+		monster.physicsBody?.contactTestBitMask = Collision().Projectile
 	}
 	
 	func randYPositionForMonsterWith(height: CGFloat) -> CGFloat {
